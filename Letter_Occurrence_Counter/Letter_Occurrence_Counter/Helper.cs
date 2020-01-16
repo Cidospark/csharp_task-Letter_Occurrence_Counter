6f2c23ca072509a783b137dfb133f5142a5bf48e
cidospark <cidospark27@gmail.com>
2020-01-16 17:09:02 +0100

    added a helper class to pick and return enlish letters occurence

---
+++ Letter_Occurrence_Counter/Letter_Occurrence_Counter/Helper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Letter_Occurrence_Counter
+{
+    class Helper
+    {
+        public static int validLettersCounts = 0;
+        public static Dictionary<char, int> util(string str)
+        {
+
+            var listOfStrings = new Dictionary<char, int>();
+            var countUniques = new Dictionary<char, int>();
+
+            foreach (char c in str)
+            {
+                if (Char.IsLetter(c))
+                {
+                    int incr = 0;
+
+                    char charLower = char.ToLower(c);
+                    if (listOfStrings.TryGetValue(charLower, out incr))
+                    {
+                        incr = listOfStrings[charLower];
+                        incr++;
+                        listOfStrings[charLower] = incr;
+                    }
+                    else
+                    {
+                        listOfStrings.Add(charLower, 1);
+                    }
+                }
+                validLettersCounts = listOfStrings.Count();
+
+            }
+
+            return listOfStrings;
+        }
+    }
+}
